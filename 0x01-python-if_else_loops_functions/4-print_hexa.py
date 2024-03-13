54b1c9df53c18e68b622338f04ba8bf4d45d747a
mosignorjude <mosignorjude2@gmail.com>
2024-03-13 14:43:22 +0100

    print numbers

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for number in range(0, 99):
-    print("{:d} = {:#x}".format(number, number)) # you can also use [hex(num)]
+    print("{:d} = {:#x}".format(number, number))  # you can also use [hex(num)]
