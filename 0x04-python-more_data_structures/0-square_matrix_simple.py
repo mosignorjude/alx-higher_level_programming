bd14ade0f912f96cca3860df73521182a35cebed
mosignorjude <mosignorjude2@gmail.com>
2024-03-20 14:25:31 +0100

    squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for rows in matrix:
+        squared_elem = [x**2 for x in rows]
+        squared_matrix.append(squared_elem)
+    return squared_matrix
