d31cc3506cb482f89f630fc37cd68d5133d2e3a6
mosignorjude <mosignorjude2@gmail.com>
2024-03-21 00:13:46 +0100

    roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rm_dict = {'I': 1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rm_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     total_num = 0
     prev_num = 0
