84a7323b5c5e23207c4e626a60eb1cf34cd03086
mosignorjude <mosignorjude2@gmail.com>
2024-04-02 23:08:30 +0100

    fix square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,6 @@
 class Square:
     """Represent a Square"""
 
-
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -15,4 +14,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size;
+        self.__size = size
