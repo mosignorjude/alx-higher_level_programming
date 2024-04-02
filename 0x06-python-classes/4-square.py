bca302d98083cc55ad689277ce68a1c7711b5b52
mosignorjude <mosignorjude2@gmail.com>
2024-04-03 00:05:03 +0100

    access and update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a Square"""
+
+    @property
+    def size(self):
+        """retrieve size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns: The area of the square.
+        """
+        return (self.__size * self.__size)
