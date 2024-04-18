c8535632591fa036eddd8240e8715db4ff1899c7
mosignorjude <mosignorjude2@gmail.com>
2024-04-18 12:45:43 +0100

    fix - TTD integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an integer addition function"""
+
+
 def add_integer(a, b=98):
     """
     Returns the integer addition of a and b.
@@ -6,7 +9,6 @@ def add_integer(a, b=98):
     Floats arguments are typecasted to to int before addition
     Raises:
         TypeError: if either a or b is a either a non-integer or non-float
-
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
