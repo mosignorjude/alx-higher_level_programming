84ee1e6818af8f4b9ad65031d15dd7d621ac93cc
mosignorjude <mosignorjude2@gmail.com>
2024-05-23 18:36:01 +0100

    root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- script that creates the MySQL server user
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEDGES ON MySQL.* user_0d_1@localhost; 
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEDGES ON *.* 'user_0d_1' @'localhost';
+FLUSH PRIVILEDGES;
