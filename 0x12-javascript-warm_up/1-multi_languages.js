7971c55cd0cff462decab128963c8cd7e9f94341
mosignorjude <mosignorjude2@gmail.com>
2024-06-20 00:51:22 +0100

    3 language js script

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firsrLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+console.log(firsrLine);
+console.log(secondLine);
+console.log(thirdLine);
