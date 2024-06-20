3f5fb87ce1e119904fd893eda6bf65ff32e406d4
mosignorjude <mosignorjude2@gmail.com>
2024-06-20 01:12:47 +0100

    js command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -10,6 +10,8 @@ const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log('No argument');
-} else {
+} else if (args.length === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
