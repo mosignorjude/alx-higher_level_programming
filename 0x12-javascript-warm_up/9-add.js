247b43e4868cc18576a2234047b40a465871936b
mosignorjude <mosignorjude2@gmail.com>
2024-06-20 03:05:14 +0100

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+console.log(add(a, b));
