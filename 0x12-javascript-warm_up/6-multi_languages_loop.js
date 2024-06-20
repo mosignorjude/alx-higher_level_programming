30d6d7146d7f05edd9cc9d0d33cc959f52746bee
mosignorjude <mosignorjude2@gmail.com>
2024-06-20 02:17:38 +0100

    loop of languages script

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let index = 0;
+
+while (index < strArray.length) {
+  console.log(strArray[index]);
+  index++;
+}
