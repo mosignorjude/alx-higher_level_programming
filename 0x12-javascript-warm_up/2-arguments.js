32f7cfa2ba6b7bbce5119ca5d9ba07498d4ab2fb
mosignorjude <mosignorjude2@gmail.com>
2024-06-20 01:29:31 +0100

    value of my argument script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /*
     * process.argv is an array where:
-    * - The first argument is the path to Node.js executable
-    * - The 2nd is the path to script file.
+    * - The first element is the path to Node.js executable
+    * - The second is the path to script file.
     * - The rest are the command line arguments
     */
 const args = process.argv.slice(2);
-// ignores the first argument that runs the scripts
+// ignores the first two elements in the process.argv array.
 
 if (args.length === 0) {
   console.log('No argument');
