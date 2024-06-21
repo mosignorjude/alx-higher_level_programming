06b05b4373320336245f270b9273206f6f8da253
mosignorjude <mosignorjude2@gmail.com>
2024-06-21 02:19:34 +0100

    fix square charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const parentSqr = require('./5-square');
+module.exports = class Square extends parentSqr {
   charPrint (c) {
     if (typeof (c) === 'undefined') {
       this.print();
