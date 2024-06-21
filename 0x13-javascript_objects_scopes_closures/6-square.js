fb8fe46233d73f57e90edcf12cd34802bcb849a1
mosignorjude <mosignorjude2@gmail.com>
2024-06-21 01:09:34 +0100

    charPrint()

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined || c !== 'C') {
+      this.print();
+    } else {
+      let sqr = '';
+      for (let i = 0; i < this.width; i++) {
+        sqr += 'C';
+      }
+      for (let j = 0; j < this.height; j++) {
+        console.log(sqr);
+      }
+    }
+  }
+};
