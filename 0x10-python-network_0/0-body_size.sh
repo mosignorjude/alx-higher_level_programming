482c4c9cece363f56b3133fc8fa2ac63df685f1b
mosignorjude <mosignorjude2@gmail.com>
2024-07-04 23:34:25 +0100

    body size task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 # script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -w "%{size_download}" "$1" 
+curl -s "$1" | wc -c 
