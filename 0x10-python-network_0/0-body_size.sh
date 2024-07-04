2d498b57bf48035fa8ae20ef264dc3d7c7a77bd9
mosignorjude <mosignorjude2@gmail.com>
2024-07-04 23:26:52 +0100

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -w "%{size_download}" https://"$1" 
+curl -w "%{size_download}" "$1" 
