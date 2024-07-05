455f80633d2a81a975d70c6fcd5733da65839510
mosignorjude <mosignorjude2@gmail.com>
2024-07-05 20:47:57 +0100

    fixed the print format in  0-htbn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ with urllib.request.urlopen(url) as response:
     body_response = response.read()
 
 print(
-    f"Body response: \n\t - type: {type(body_response)}\n\
-    \t - content: {body_response}\n\
-    \t - utf8 content: {body_response.decode('utf-8')}")
+    f"Body response: \n\t- type: {type(body_response)}\n\
+    \t- content: {body_response}\n\
+    \t- utf8 content: {body_response.decode('utf-8')}")
