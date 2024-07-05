c1d2dfd66a54782c670b95c5b8507fcab921add6
mosignorjude <mosignorjude2@gmail.com>
2024-07-05 21:53:18 +0100

    minor in header response file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,9 +4,10 @@ script that takes in a URL,
 sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.
 """
+import urllib.request
+import sys
+
 if __name__ == '__main__':
-    import urllib.request
-    import sys
 
     request = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(request) as response:
