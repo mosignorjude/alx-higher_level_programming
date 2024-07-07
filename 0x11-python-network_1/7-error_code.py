0c5902ea0c59712bbfda93a52f5840689ec946e0
mosignorjude <mosignorjude2@gmail.com>
2024-07-07 17:48:27 +0100

    added print status code - Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     r = requests.get(url)
     if r.status_code >= 400:
         print('Error code: {}'.format(r.status_code))
-    print(r.status_code)
+    else:
+        print(r.status_code)
