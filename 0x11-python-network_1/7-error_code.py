5d6cfac4857f8ecb9e11b033f3a0470faf278dc3
mosignorjude <mosignorjude2@gmail.com>
2024-07-07 17:44:56 +0100

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,15 +4,10 @@ script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 """
 import requests
-import urllib.error
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            content = response.read()
-            print(content.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
