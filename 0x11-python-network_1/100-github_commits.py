c96fe7cae21af049a9fa0a687ff750b3ae8f2dd8
mosignorjude <mosignorjude2@gmail.com>
2024-07-08 15:55:02 +0100

    10. Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner, repo)
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
